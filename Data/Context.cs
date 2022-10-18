3d8b20b38cfa123a8fec7835dc5f86c3cbc74597
Andreza Pessoa <andreza.21pessoa@gmail.com>
2022-10-17 22:41:29 -0300

    tp 02

---
+++ Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TP02.Models;
+
+namespace TP02.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        { }
+
+        public DbSet<Bl> Bls { get; set; }
+
+        public DbSet<Container> Containers { get; set; }
+    }
+}
